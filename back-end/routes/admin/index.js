2168c344761bbc94a738c10fb05c15c253db6cb6
Jesse Wu <Jessewu1999@gmail.com>
2023-02-07 21:39:20 -0600

    remove console.log and formatted the file

--- back-end/routes/admin/index.js
+++ back-end/routes/admin/index.js
@@ -4,43 +4,44 @@ import User from "../../models/User.js";
 import cache from "../../cache.js";
 
 router.post("/sendRecords", async (req, res, next) => {
-    // check if the username exist, then return user data
     try {
-        
+
         var errorMessage = "";
         let { location, records } = req.body;
-        for(var record of records) {
-            let user = await User.findOne({ userName: record.userName });
-            if(user !== null){
-                if(record.amount < user.accountBalance){
-                    user.accountBalance -= record.amount;
 
+        //Iterate through the transaction records list and update database
+        for (var record of records) {
+            let user = await User.findOne({ userName: record.userName });
+            if (user !== null) {
+                if (record.amount < user.accountBalance) {
+
+                    user.accountBalance -= record.amount;
                     user.expenseHistory.push({
-                        location : location,
-                        date : record.date,
-                        category : record.category,
-                        amount : record.amount
+                        location: location,
+                        date: record.date,
+                        category: record.category,
+                        amount: record.amount
                     });
 
                     await user.save();
-                }else{
+
+                } else {
                     errorMessage += record.userName + " account balance not enough\n";
-                    console.log("1" + errorMessage);
                 }
-            }else{
+            } else {
                 errorMessage += record.userName + " not exists\n";
             }
         }
 
-        console.log("2" + errorMessage);
-        if(errorMessage === ""){
+        if (errorMessage === "") {
             res.status(200).send("Successfully processed records.");
-        }else{
+        } else {
             res.status(400).send("Errors:\n" + errorMessage);
         }
-    } catch (err) {
-      res.status(500).json({ ErrorMessage: err.message });
-    }
-  });
 
-  export default router;
+    } catch (err) {
+        res.status(500).json({ ErrorMessage: err.message });
+    }
+});
+
+export default router;
