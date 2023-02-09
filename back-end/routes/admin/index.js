1c378ef4f4cdeaebebc6ae562e8ac3495af77eb3
Jesse Wu <Jessewu1999@gmail.com>
2023-02-09 11:48:01 -0600

    Change tab to two space instead of four

--- back-end/routes/admin/index.js
+++ back-end/routes/admin/index.js
@@ -4,44 +4,44 @@ import User from "../../models/User.js";
 import cache from "../../cache.js";
 
 router.post("/sendRecords", async (req, res, next) => {
-    try {
+  try {
 
-        var errorMessage = "";
-        let { location, records } = req.body;
+    var errorMessage = "";
+    let { location, records } = req.body;
 
-        //Iterate through the transaction records list and update database
-        for (var record of records) {
-            let user = await User.findOne({ userName: record.userName });
-            if (user !== null) {
-                if (record.amount < user.accountBalance) {
+    //Iterate through the transaction records list and update database
+    for (var record of records) {
+      let user = await User.findOne({ userName: record.userName });
+      if (user !== null) {
+        if (record.amount < user.accountBalance) {
 
-                    user.accountBalance -= record.amount;
-                    user.expenseHistory.push({
-                        location: location,
-                        date: record.date,
-                        category: record.category,
-                        amount: record.amount
-                    });
+          user.accountBalance -= record.amount;
+          user.expenseHistory.push({
+            location: location,
+            date: record.date,
+            category: record.category,
+            amount: record.amount
+          });
 
-                    await user.save();
+          await user.save();
 
-                } else {
-                    errorMessage += record.userName + " account balance not enough\n";
-                }
-            } else {
-                errorMessage += record.userName + " not exists\n";
-            }
-        }
-
-        if (errorMessage === "") {
-            res.status(200).send("Successfully processed records.");
         } else {
-            res.status(400).send("Errors:\n" + errorMessage);
+          errorMessage += record.userName + " account balance not enough\n";
         }
-
-    } catch (err) {
-        res.status(500).json({ ErrorMessage: err.message });
+      } else {
+        errorMessage += record.userName + " not exists\n";
+      }
     }
+
+    if (errorMessage === "") {
+      res.status(200).send("Successfully processed records.");
+    } else {
+      res.status(400).send("Errors:\n" + errorMessage);
+    }
+
+  } catch (err) {
+    res.status(500).json({ ErrorMessage: err.message });
+  }
 });
 
 export default router;
