14497dcfef6b665a5c1df165fd0d3a59ecc94304
Afaz <afaz.munshi49@gmail.com>
2023-02-10 15:47:36 -0600

    Removed total income cards

--- front-end/src/views/dashboard/Default/index.js
+++ front-end/src/views/dashboard/Default/index.js
@@ -30,16 +30,6 @@ const Dashboard = () => {
                     <Grid item lg={4} md={6} sm={6} xs={12}>
                         <TotalOrderLineChartCard isLoading={isLoading} />
                     </Grid>
-                    <Grid item lg={4} md={12} sm={12} xs={12}>
-                        <Grid container spacing={gridSpacing}>
-                            <Grid item sm={6} xs={12} md={6} lg={12}>
-                                <TotalIncomeDarkCard isLoading={isLoading} />
-                            </Grid>
-                            <Grid item sm={6} xs={12} md={6} lg={12}>
-                                <TotalIncomeLightCard isLoading={isLoading} />
-                            </Grid>
-                        </Grid>
-                    </Grid>
                 </Grid>
             </Grid>
             <Grid item xs={12}>
