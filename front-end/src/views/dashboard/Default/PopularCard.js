817b53945ea6c641d0181260ca60b48787ab78ca
mike91827 <mike91827.2015@gmail.com>
2023-02-14 16:20:12 -0600

    https://github.com/namhai923/Bison-Bank-of-Canada/issues/31

--- front-end/src/views/dashboard/Default/PopularCard.js
+++ front-end/src/views/dashboard/Default/PopularCard.js
@@ -21,7 +21,7 @@ import ChevronRightOutlinedIcon from '@mui/icons-material/ChevronRightOutlined';
 import MoreHorizOutlinedIcon from '@mui/icons-material/MoreHorizOutlined';
 import KeyboardArrowUpOutlinedIcon from '@mui/icons-material/KeyboardArrowUpOutlined';
 import KeyboardArrowDownOutlinedIcon from '@mui/icons-material/KeyboardArrowDownOutlined';
-
+import { createTheme } from '@mui/material/styles';
 // ==============================|| DASHBOARD DEFAULT - POPULAR CARD ||============================== //
 
 const PopularCard = ({ isLoading }) => {
@@ -36,13 +36,44 @@ const PopularCard = ({ isLoading }) => {
     const handleClose = () => {
         setAnchorEl(null);
     };
-    function createData(transNumber, merchant, date, category, price) {
-        return { transNumber, merchant, date, category, price };
-    }
+    const themeColor = createTheme({
+        palette: {
+            yellow: {
+                light: '#ffa726',
+                main: '#f57c00',
+                dark: '#FFFF66',
+                contrastText: 'rgba(255, 255, 102, 255)'
+            },
+            contrastThreshold: 3,
+            // Used by the functions below to shift a color's luminance by approximately
+            // two indexes within its tonal palette.
+            // E.g., shift from Red 500 to Red 300 or Red 700.
+            tonalOffset: 0.2
+        }
+    });
 
+    function createData(transNumber, merchant, date, category, price) {
+        var color;
+        var background;
+        if (price <= 10) {
+            //(color = theme.palette.success.dark), (background = theme.palette.success.light);
+            (color = theme.palette.primary.light), (background = theme.palette.success.dark);
+        } else if (price >= 50) {
+            (color = theme.palette.primary.light), (background = theme.palette.error.dark);
+        } else {
+            //(color = theme.palette.success.dark), (background = theme.palette.success.light);
+            (color = theme.palette.primary.light), (background = themeColor.palette.yellow.dark);
+        }
+        if (price % 1 == 0) {
+            price = price + '.00';
+        }
+        price = '$' + price;
+        return { transNumber, merchant, date, category, price, color, background };
+    }
     const rows = [
-        createData(15432765, 'Walmart', 'February 14, 2019', 'Groceries', '$44.00'),
-        createData(15432434524323, 'Superstore', 'May 14, 29', 'Eye Test', '$400000000.00')
+        createData(15432765, 'Walmart', 'February 14, 2019', 'Groceries', 5),
+        createData(15432434524323, 'Superstore', 'May 14, 29', 'Eye Test', 400000000.06),
+        createData(15432434524323, 'Superstore', 'May 14, 29', 'Eye Test', 40.06)
     ];
     return (
         <>
@@ -77,6 +108,19 @@ const PopularCard = ({ isLoading }) => {
                                             <TableCell align="left">{row.date}</TableCell>
                                             <TableCell align="left">{row.category}</TableCell>
                                             <TableCell align="left">{row.price}</TableCell>
+                                            <Avatar
+                                                variant="rounded"
+                                                sx={{
+                                                    width: 16,
+                                                    height: 16,
+                                                    borderRadius: '5px',
+                                                    backgroundColor: row.background,
+                                                    color: row.color,
+                                                    ml: 2
+                                                }}
+                                            >
+                                                <KeyboardArrowDownOutlinedIcon fontSize="small" color="inherit" />
+                                            </Avatar>
                                         </TableRow>
                                     ))}
                                 </TableBody>
