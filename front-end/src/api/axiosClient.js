1641aa06e1240e74cc0d917e161621c9394410f7
Hai Nguyen <namhainguyenminh@gmail.com>
2023-02-14 19:05:26 -0600

    #41 - remove generated files

---
+++ front-end/src/api/axiosClient.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import queryString from 'query-string';
+
+let axiosClient = axios.create({
+    // baseURL: process.env.BBC_API,
+    baseURL: 'http://localhost:8000',
+    headers: {
+        'content-type': 'application/json'
+    },
+    paramSerializer: (params) => queryString.stringify(params)
+});
+
+axiosClient.interceptors.response.use(
+    (response) => {
+        if (response && response.data) {
+            return response.data;
+        }
+        return response;
+    },
+    (error) => {
+        throw error;
+    }
+);
+
+export default axiosClient;
--- front-end/src/layout/MainLayout/Header/WelcomeSection/index.js
+++ front-end/src/layout/MainLayout/Header/WelcomeSection/index.js
@@ -1,8 +1,15 @@
-import { useTheme } from '@mui/material/styles';
+import React, { useState, useEffect } from 'react';
+
+import bbcApi from '../../../../api/bbcApi.js';
 
 const WelcomeMessage = () => {
-    const theme = useTheme();
-    var name = 'Bison';
+    let [name, setName] = useState('Bison');
+
+    useEffect(async () => {
+        let user = await bbcApi.getUser('elonmusk@twitter.com');
+        setName(user.firstName);
+    }, []);
+    console.log(name);
     return <h1 style={{ paddingLeft: '25px' }}> Welcome, {name}!</h1>;
 };
 
