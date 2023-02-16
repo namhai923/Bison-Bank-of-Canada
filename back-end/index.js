edff04585fd61e387a62f92b1db7782730ddb6f8
LimitlessJesse <jessewu1999@gmail.com>
2023-02-16 02:07:06 -0600

    console log port

--- back-end/index.js
+++ back-end/index.js
@@ -1,7 +1,7 @@
 const { PORT } = require("./src/config/index.config");
 
 const app = require("./src/app");
-
+console.log(PORT);
 var server = app.listen(PORT, (err) => {
   if (err) {
     return console.error(err);
