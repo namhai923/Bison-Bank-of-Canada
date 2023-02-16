8d4901873ce31c8acd5e53283ae0c195114cca98
Jesse Wu <Jessewu1999@gmail.com>
2023-02-15 21:04:06 -0600

    Added unit tests for couple endpoints

--- back-end/src/config/index.config.js
+++ back-end/src/config/index.config.js
@@ -1,6 +1,10 @@
 const dotenv = require("dotenv");
 
 dotenv.config();
-let { PORT, DB_URL } = process.env;
+let { PORT, DB_URL, TESTDB_URL } = process.env;
+var exportDB = DB_URL;
+if(process.env.NODE_ENV === 'test'){
+  exportDB = TESTDB_URL;
+}
 
-module.exports = { PORT, DB_URL };
+module.exports = { PORT, exportDB };
