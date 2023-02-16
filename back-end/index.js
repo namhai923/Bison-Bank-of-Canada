2b5ecfbbdf9f501bb86c97d7ce77a03147050454
Yixi <wuy8@myumanitoba.ca>
2023-02-16 00:42:52 -0600

    change port to test

--- back-end/index.js
+++ back-end/index.js
@@ -2,11 +2,11 @@ const { PORT } = require("./src/config/index.config");
 
 const app = require("./src/app");
 
-var server = app.listen(PORT, (err) => {
+var server = app.listen(3000, (err) => {
   if (err) {
     return console.error(err);
   }
-  console.log("Server is running, listening to port %d", PORT);
+  console.log("Server is running, listening to port %d", 3000);
 });
 
 module.exports = server;
