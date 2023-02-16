4502622b80cd7fdc044e8d7ef82f015900924366
LimitlessJesse <jessewu1999@gmail.com>
2023-02-16 01:54:09 -0600

    Add comment in pipeline

--- back-end/src/config/index.config.js
+++ back-end/src/config/index.config.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 let { PORT, DB_URL, TESTDB_URL } = process.env;
-console.log(process.env);
+
 var exportDB = DB_URL;
 if(process.env.NODE_ENV === 'test'){
   exportDB = TESTDB_URL;
