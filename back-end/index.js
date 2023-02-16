e638e6169448ad502e79d2f8cf3cc0b43b832e2c
LimitlessJesse <jessewu1999@gmail.com>
2023-02-16 02:43:44 -0600

    Setup pipeline with secret

--- back-end/index.js
+++ back-end/index.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = require("./src/app");
-console.log(process.env);
+
 var server = app.listen(PORT, (err) => {
   if (err) {
     return console.error(err);
