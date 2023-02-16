7ce4758e916030b8a5479350373607fdcf9a2568
Hai Nguyen <namhainguyenminh@gmail.com>
2023-02-15 22:54:01 -0600

    add some front end unit tests

--- front-end/src/layout/MainLayout/Header/WelcomeSection/index.js
+++ front-end/src/layout/MainLayout/Header/WelcomeSection/index.js
@@ -1,19 +1,15 @@
 import React, { useState, useEffect } from 'react';
 
-import bbcApi from '../../../../api/bbcApi.js';
+// import bbcApi from '../../../../api/bbcApi.js';
 
 const WelcomeMessage = () => {
     let [name, setName] = useState('Bison');
 
-    useEffect(() => {
-        async function getUser() {
-            let user = await bbcApi.getUser('elonmusk@twitter.com');
-            setName(user.firstName);
-            console.log(user);
-        }
-        getUser();
-    }, []);
-    console.log('asd' + bbcApi.getUser('elonmusk@twitter.com'));
+    // console.log(name);
+    // }, []);
+    //     setName(user.firstName);
+    //     let user = await bbcApi.getUser('elonmusk@twitter.com');
+    // useEffect(async () => {
     return <h1 style={{ paddingLeft: '25px' }}> Welcome, {name}!</h1>;
 };
 
