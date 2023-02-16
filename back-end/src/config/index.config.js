43ba89c4fb56574585f3d0feb5ff2c66b423d3f7
Yixi <wuy8@myumanitoba.ca>
2023-02-16 00:51:12 -0600

    console log for testing

--- back-end/src/config/index.config.js
+++ back-end/src/config/index.config.js
@@ -1,6 +1,7 @@
 const dotenv = require("dotenv");
 
 dotenv.config();
+console.log(process.env);
 let { PORT, DB_URL, TESTDB_URL } = process.env;
 var exportDB = DB_URL;
 if(process.env.NODE_ENV === 'test'){
