41e5d4128506295f1dd5d69c323f8a03098e9eae
mike91827 <mike91827.2015@gmail.com>
2023-03-04 18:23:11 -0600

    added UI for transfer money

---
+++ front-end/src/views/pages/authentication/auth-template/TransferMoney.js
@@ -0,0 +1,91 @@
+import { Link } from 'react-router-dom';
+
+// material-ui
+import { useTheme } from '@mui/material/styles';
+import { Divider, Grid, Stack, Typography, useMediaQuery } from '@mui/material';
+
+// project imports
+import AuthWrapper from '../AuthWrapper';
+import AuthCardWrapper from '../AuthCardWrapper';
+import AuthLogin from '../auth-forms/AuthLogin';
+import AuthTransferMoney from '../auth-forms/AuthTransferMoney';
+import Logo from 'ui-component/Logo';
+import AuthFooter from 'ui-component/cards/AuthFooter';
+
+// assets
+
+// ================================|| AUTH3 - LOGIN ||================================ //
+
+const Login = () => {
+    const theme = useTheme();
+    const matchDownSM = useMediaQuery(theme.breakpoints.down('md'));
+
+    return (
+        <AuthWrapper>
+            <Grid container direction="column" justifyContent="flex-end" sx={{ minHeight: '100vh' }}>
+                <Grid item xs={12}>
+                    <Grid container justifyContent="center" alignItems="center" sx={{ minHeight: 'calc(100vh - 68px)' }}>
+                        <Grid item sx={{ m: { xs: 1, sm: 3 }, mb: 0 }}>
+                            <AuthCardWrapper>
+                                <Grid container spacing={2} alignItems="center" justifyContent="center">
+                                    <Grid item sx={{ mb: 0 }}>
+                                        <Logo />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <Grid
+                                            container
+                                            direction={matchDownSM ? 'column-reverse' : 'row'}
+                                            alignItems="center"
+                                            justifyContent="center"
+                                        >
+                                            <Grid item>
+                                                <Stack alignItems="center" justifyContent="center" spacing={1}>
+                                                    <Typography
+                                                        color={theme.palette.secondary.main}
+                                                        gutterBottom
+                                                        variant={matchDownSM ? 'h3' : 'h2'}
+                                                        textAlign="center"
+                                                    >
+                                                        Transfering Money Has Never Been So Simple
+                                                    </Typography>
+                                                    <Typography
+                                                        variant="caption"
+                                                        fontSize="16px"
+                                                        align="center"
+                                                        //textAlign={matchDownSM ? 'center' : 'inherit'}
+                                                        textAlign="center"
+                                                    >
+                                                        Enter the Credentials of the Person you want to make a Transaction with and the
+                                                        Amount of Money You Want to Transfer
+                                                    </Typography>
+                                                </Stack>
+                                            </Grid>
+                                        </Grid>
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <AuthTransferMoney />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <Divider />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <Grid item container direction="column" alignItems="center" xs={12}>
+                                            <Typography component={Link} to="/" variant="subtitle1" sx={{ textDecoration: 'none' }}>
+                                                Back to Dashboard
+                                            </Typography>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                            </AuthCardWrapper>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item xs={12} sx={{ m: 3, mt: 1 }}>
+                    <AuthFooter />
+                </Grid>
+            </Grid>
+        </AuthWrapper>
+    );
+};
+
+export default Login;
