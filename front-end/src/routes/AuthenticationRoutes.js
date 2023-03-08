10f3c6bd7cb09a525e878fbc5ba8740a40d476fa
Hai <namhainguyenminh@gmail.com>
2023-03-07 19:27:40 -0600

    minor changes

--- front-end/src/routes/AuthenticationRoutes.js
+++ front-end/src/routes/AuthenticationRoutes.js
@@ -2,7 +2,6 @@ import { lazy } from 'react';
 
 // project imports
 import Loadable from 'ui-component/Loadable';
-import MainLayout  from 'layout/MainLayout';
 // login option 3 routing
 const AuthLogin = Loadable(lazy(() => import('views/pages/authentication/auth-template/Login')));
 const AuthRegister = Loadable(lazy(() => import('views/pages/authentication/auth-template/Register')));
@@ -12,7 +11,6 @@ const TransferHistory = Loadable(lazy(() => import('views/pages/authentication/T
 
 const AuthenticationRoutes = {
     path: '/',
-    element: <MinimalLayout />,
     children: [
         {
             path: '/user/login',
