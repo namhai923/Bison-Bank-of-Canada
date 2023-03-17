a06a6c2e7d6f26c2478c2ab83400f01df5f0de3f
Afaz <afaz.munshi49@gmail.com>
2023-03-16 23:09:36 -0500

    resolved merge conflicts

--- front-end/src/views/banking/history/TransferHistory.js
+++ front-end/src/views/banking/history/TransferHistory.js
@@ -1,12 +1,28 @@
 import { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import ListCard from 'ui-component/cards/ListCard';
 import createData from 'utils/createData';
+import bbcApi from '../../../api/bbcApi';
+import { setUser } from '../../authentication/userSlice';
 
 const TransferHistory = () => {
     let userInfo = useSelector((state) => state.user);
     let filterInfo = useSelector((state) => state.filter);
+
+    let dispatch = useDispatch();
+
+    window.addEventListener('load', async () => {
+        if (userInfo === null || userInfo.userName === '') return;
+
+        try {
+            let user = await bbcApi.getUser(userInfo.userName);
+            let action = setUser(user);
+            dispatch(action);
+        } catch (error) {
+            console.log(error);
+        }
+    });
 
     let labels = ['Transfer Id', ' ', 'Receiver/Recipeint', 'Date', 'Amount'];
     let filterLabels = [
@@ -43,7 +59,7 @@ const TransferHistory = () => {
                 return { ...row, transNumber: counter++ };
             });
         setRows(displayRows);
-    }, [filterInfo.sender, filterInfo, userInfo]);
+    }, [filterInfo, userInfo]);
 
     return (
         <ListCard
